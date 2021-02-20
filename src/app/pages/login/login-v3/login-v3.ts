e845db76625ea302f3bfee825433c1c94316e421
Bruno <bruno.okafor@gmail.com>
2021-02-20 08:30:28 +0100

    Added Home page

--- src/app/pages/login/login-v3/login-v3.ts
+++ src/app/pages/login/login-v3/login-v3.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
-import { NgForm } from '@angular/forms';
 import pageSettings from '../../../config/page-settings';
+import { User } from '../../../model/user';
 
 @Component({
   selector: 'login-v3',
@@ -10,10 +10,15 @@ import pageSettings from '../../../config/page-settings';
 
 export class LoginV3Page implements OnDestroy {
   pageSettings = pageSettings;
+  email: string;
+  password: string;
+  user: User;
+  users: User[] = [];
 
   constructor(private router: Router, private renderer: Renderer2) {
     this.pageSettings.pageEmpty = true;
     this.renderer.addClass(document.body, 'bg-white');
+    this.createUsers();
   }
 
   ngOnDestroy() {
@@ -21,7 +26,29 @@ export class LoginV3Page implements OnDestroy {
     this.renderer.removeClass(document.body, 'bg-white');
   }
 
-  formSubmit(f: NgForm) {
-    this.router.navigate(['dashboard/v3']);
+  login() {
+    
+    // this.router.navigate(['dashboard/v3']);
+  }
+
+  createUsers() {
+    if (localStorage.getItem('users') == null) {
+      this.user = new User();
+      this.user.company = 'First Brokers Limited';
+      this.user.email = 'firstbrokers@gmail.com';
+      this.user.password = 'password';
+      this.user.role = 'Broker';
+      this.users.push(this.user);
+
+      this.user = new User();
+      this.user.company = 'Old Mutual Limited';
+      this.user.email = 'oldmutual@gmail.com';
+      this.user.password = 'password';
+      this.user.role = 'Admin';
+      this.users.push(this.user);
+
+      localStorage.setItem('users', JSON.stringify(this.users));
+      
+    }
   }
 }
